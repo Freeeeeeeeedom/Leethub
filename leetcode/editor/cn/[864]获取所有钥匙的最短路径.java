76ec6abf516e9d25c0dd52f292b569194ed25ac1
Freeeeeeeeedom <709034836@qq.com>
2022-11-11 21:42:02 +0800

    auto update

--- leetcode/editor/cn/[864]获取所有钥匙的最短路径.java
+++ leetcode/editor/cn/[864]获取所有钥匙的最短路径.java
@@ -60,9 +60,53 @@
 // Related Topics 位运算 广度优先搜索 数组 矩阵 👍 148 👎 0
 
 
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Deque;
+
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
+    static int N = 35, K = 10, INF = 0x3f3f3f3f;
+    static int[][][] dist = new int[N][N][1 << K];
+    static  int[][] dirs = new int[][]{{1,0},{-1,0},{0,1},{0,-1}};
+
     public int shortestPathAllKeys(String[] grid) {
+        int n = grid.length;
+        int m = grid[0].length();
+        int cnt = 0;
+        Deque<int[]> d = new ArrayDeque<>();
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                Arrays.fill(dist[i][j],INF);
+                char c = grid[i].charAt(j);
+                if(c=='@'){
+                    //起始位置入栈
+                    d.addLast(new int[]{i,j,0});
+                    dist[i][j][0] = 0;
+                }
+                else if(c >= 'a' && c <= 'z') cnt++;
+            }
+        }
+
+        while(!d.isEmpty()){
+            int[] info = d.pollFirst();
+            int x = info[0], y = info[1], cur = info[2], step = dist[x][y][cur];
+            for(int[] di : dirs){
+                int nx = x + di[0], ny = y + di[1];
+                if(nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
+                char c = grid[nx].charAt(ny);
+                if(c == '#') continue;
+                if((c >= 'A' && c <= 'Z') && ((cur >> (c - 'A') & 1) == 0)) continue;
+                int ncur = cur;
+                if(c >= 'a' && c <= 'z') ncur = ncur | (1 << (c - 'a'));
+                if(ncur == (1 << cnt) - 1) return step + 1;
+                if(step + 1 >= dist[nx][ny][ncur]) continue;
+                dist[nx][ny][ncur] = step + 1;
+                d.addLast(new int[]{nx,ny,ncur});
+            }
+        }
+
+        return -1;
 
     }
 }
