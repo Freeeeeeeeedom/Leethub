2af613c9b830b40bf41681a84b96422c17c257c0
Freeeeeeeeedom <709034836@qq.com>
2022-11-26 21:42:03 +0800

    auto update

--- leetcode/editor/cn/[784]字母大小写全排列.java
+++ leetcode/editor/cn/[784]字母大小写全排列.java
@@ -30,6 +30,7 @@
 // Related Topics 位运算 字符串 回溯 👍 437 👎 0
 
 
+import com.sun.org.apache.xpath.internal.operations.String;
 import com.sun.source.tree.Tree;
 
 import javax.swing.tree.TreeNode;
@@ -37,30 +38,51 @@ import java.util.*;
 
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
+//    public List<String> letterCasePermutation(String s) {
+//        List<String> ans = new ArrayList<>();
+//        traceback(s,ans,0);
+//        return ans;
+//    }
+//    public void traceback(String s,List<String> ans,int index){
+//        if(index == s.length()) return;
+//        if(Character.isDigit(s.charAt(index))){
+//            traceback(s,ans,index+1);
+//        }
+//
+//        if(!ans.contains(s)) ans.add(s);
+//        traceback(s,ans,index+1);
+//        char i = s.charAt(index);
+//        if(i>='a' && i<='z'){
+//            i = Character.toUpperCase(i);
+//        }
+//        else i = Character.toLowerCase(i);
+//        String changed = s.substring(0,index)+i+s.substring(index+1);
+//        if(!ans.contains(changed)) ans.add(changed);
+//        traceback(changed,ans,index+1);
+//    }
+
     public List<String> letterCasePermutation(String s) {
         List<String> ans = new ArrayList<>();
-        traceback(s,ans,0);
-        return ans;
-    }
-    public void traceback(String s,List<String> ans,int index){
-        if(index == s.length()) return;
-        if(Character.isDigit(s.charAt(index))){
-            traceback(s,ans,index+1);
-        }
-
-        if(!ans.contains(s)) ans.add(s);
-        traceback(s,ans,index+1);
-        char i = s.charAt(index);
-        if(i>='a' && i<='z'){
-            i = Character.toUpperCase(i);
-        }
-        else i = Character.toLowerCase(i);
-        String changed = s.substring(0,index)+i+s.substring(index+1);
-        if(!ans.contains(changed)) ans.add(changed);
-        traceback(changed,ans,index+1);
+        StringBuilder cur = new StringBuilder();
+        int index = 0;
+        traceBack(ans,cur,0,s);
+        return  ans;
     }
 
-
+    public void traceBack(List<String> ans,StringBuilder cur, int index,String s){
+        if(cur.length() == s.length()){ ans.add(new String(cur.toString())); return;}
+        if(!Character.isDigit(s.charAt(index))){
+            StringBuilder temp = new StringBuilder(cur.toString());
+            temp.append(Character.toLowerCase(s.charAt(index)));
+            traceBack(ans,temp,index+1,s);
+            cur.append(Character.toUpperCase(s.charAt(index)));
+            traceBack(ans,cur,index+1,s);
+        }
+        else{
+            cur.append(s.charAt(index));
+            traceBack(ans,cur,index+1,s);
+        }
+    }
 
 }
 //leetcode submit region end(Prohibit modification and deletion)
