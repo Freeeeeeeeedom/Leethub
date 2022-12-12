1170cdf5e3412ef3399ab77433a96ef2c09c7fb4
Freeeeeeeeedom <709034836@qq.com>
2022-12-12 21:42:01 +0800

    auto update

---
+++ leetcode/editor/cn/[1781]所有子字符串美丽值之和.java
@@ -0,0 +1,59 @@
+//一个字符串的 美丽值 定义为：出现频率最高字符与出现频率最低字符的出现次数之差。 
+//
+// 
+// 比方说，"abaacc" 的美丽值为 3 - 1 = 2 。 
+// 
+//
+// 给你一个字符串 s ，请你返回它所有子字符串的 美丽值 之和。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：s = "aabcb"
+//输出：5
+//解释：美丽值不为零的字符串包括 ["aab","aabc","aabcb","abcb","bcb"] ，每一个字符串的美丽值都为 1 。 
+//
+// 示例 2： 
+//
+// 
+//输入：s = "aabcbaa"
+//输出：17
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= s.length <= 500 
+// s 只包含小写英文字母。 
+// 
+//
+// Related Topics 哈希表 字符串 计数 👍 29 👎 0
+
+
+import java.sql.Array;
+import java.util.Arrays;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int beautySum(String s) {
+        char[] str = s.toCharArray();
+        int n = str.length;
+        int[] cnt = new int[26];
+
+        int ans = 0;
+        int l = 0;
+        int r = 0;
+
+        int max 
+        while(r < n){
+            cnt[cnt[r]-'a']++;
+
+        }
+        return max - min;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
