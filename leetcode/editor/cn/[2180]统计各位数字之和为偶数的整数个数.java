4674fa76cffc0300dd56092e65e8437c1ea936b6
Freeeeeeeeedom <709034836@qq.com>
2023-01-06 21:42:02 +0800

    auto update

---
+++ leetcode/editor/cn/[2180]统计各位数字之和为偶数的整数个数.java
@@ -0,0 +1,57 @@
+//给你一个正整数 num ，请你统计并返回 小于或等于 num 且各位数字之和为 偶数 的正整数的数目。 
+//
+// 正整数的 各位数字之和 是其所有位上的对应数字相加的结果。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：num = 4
+//输出：2
+//解释：
+//只有 2 和 4 满足小于等于 4 且各位数字之和为偶数。    
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：num = 30
+//输出：14
+//解释：
+//只有 14 个整数满足小于等于 30 且各位数字之和为偶数，分别是： 
+//2、4、6、8、11、13、15、17、19、20、22、24、26 和 28 。
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= num <= 1000 
+// 
+//
+// Related Topics 数学 模拟 👍 34 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int countEven(int num) {
+        int count = 0;
+        while(num>0){
+            if(check(num)) count++;
+            num--;
+        }
+        return count;
+    }
+
+    public boolean check(int n){
+        int val = 0;
+        while(n > 0){
+            val += n % 10;
+            n /= 10;
+        }
+        return val%2 == 0;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
